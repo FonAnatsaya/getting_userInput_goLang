d35e542305bc88de126d618500fb3e8b6abb97a8
fon <anatsaya.lee@gmail.com>
2024-06-04 16:19:12 +0700

    Create saver and outputtable interfaces

--- main.go
+++ main.go
@@ -7,26 +7,55 @@ import (
 	"strings"
 
 	"example.com/note/note"
+	"example.com/note/todo"
 )
+
+type saver interface {
+	Save() error
+}
+
+type outputtable interface {
+	saver
+	Display()
+}
 
 func main() {
 	title, content := getNoteData()
-	userNote, err := note.New(title, content)
+	todoText := getUserInput("Todo text: ")
 
+	todo, err := todo.New(todoText)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	userNote.Display()
-	err = userNote.Save()
-
+	userNote, err := note.New(title, content)
 	if err != nil {
-		fmt.Println("Saving the note failed.")
+		fmt.Println(err)
 		return
 	}
 
-	fmt.Println("Saving the note succeeded!")
+	err = outputData(todo)
+	if err != nil {
+		return
+	}
+
+	outputData(userNote)
+}
+
+func outputData(data outputtable) error {
+	data.Display()
+	return saveData(data)
+}
+
+func saveData(data saver) error {
+	err := data.Save()
+	if err != nil {
+		fmt.Println("Saving the todo failed.")
+		return err
+	}
+	fmt.Println("Saving the todo succeeded!")
+	return nil
 }
 
 func getNoteData() (string, string) {
